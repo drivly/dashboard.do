e1fc27f76b50a3bae2f08c3c6ec0e103b6b1e538
Chris Risner <crisner.prsvr@gmail.com>
2022-10-19 13:29:51 -0500

    cleaned up DataProvider getList & getManyReference

--- src/utils/dataProvider.js
+++ src/utils/dataProvider.js
@@ -6,16 +6,13 @@ const httpClient = fetchUtils.fetchJson
 
 export default {
   getList: async (resource, params) => {
-    // console.log('fetchDataCategory', resource, params)
     const { page, perPage } = params.pagination
-    const { field, order } = params.sort
+    // const { field, order } = params.sort
     const baseurl = `${apiUrl}${resource}`
 
     const { json } = await httpClient(baseurl)
-    // console.log("initial JSON", json)
     let records = []
     const totalDocs = await json.data.length
-    const total = Math.ceil(totalDocs / perPage)
 
     if (page === 1) {
       const initialUrl = `${apiUrl}${resource}?limit=${perPage}`
@@ -27,16 +24,6 @@ export default {
       }`
       records = await httpClient(nextUrl)
     }
-    // console.log('records', records)
-    // const query = {
-    //   sort: JSON.stringify([field, order]),
-    //   range: JSON.stringify([(page - 1) * perPage, page * perPage - 1]),
-    //   filter: JSON.stringify(params.filter),
-    // }
-    // console.log(query)
-    // const url = `${apiUrl}${resource}?${stringify(query)}`
-
-    // const { json } = await httpClient(url)
     return {
       data: records.json.data,
       total: parseInt(totalDocs),
@@ -56,22 +43,26 @@ export default {
 
   getManyReference: async (resource, params) => {
     const { page, perPage } = params.pagination
-    const { field, order } = params.sort
+    // const { field, order } = params.sort
+    const baseurl = `${apiUrl}${resource}`
 
-    const query = {
-      sort: JSON.stringify([field, order]),
-      range: JSON.stringify([(page - 1) * perPage, page * perPage - 1]),
-      filter: JSON.stringify({
-        ...params.filter,
-        [params.target]: params.id,
-      }),
+    const { json } = await httpClient(baseurl)
+    let records = []
+    const totalDocs = await json.data.length
+
+    if (page === 1) {
+      const initialUrl = `${apiUrl}${resource}?limit=${perPage}`
+      records = await httpClient(initialUrl)
+    } else {
+      const skip = perPage * (page - 1)
+      const nextUrl = `${apiUrl}${resource}?skip=${skip}&limit=${
+        perPage + skip
+      }`
+      records = await httpClient(nextUrl)
     }
-    const url = `${apiUrl}${resource}?${stringify(query)}`
-
-    const { json } = await httpClient(url)
     return {
-      data: json.data,
-      total: parseInt(json.data.length),
+      data: records.json.data,
+      total: parseInt(totalDocs),
     }
   },
 
